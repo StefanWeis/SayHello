eb027b8a8fba530cb26a9182509e9aa737eae1ea
stefanweis01 <stefanweis01@gmail.com>
2015-04-23 15:37:30 +0200

    Initial commit

---
+++ app/src/main/java/de/dma/sayhello/StartActivity.java
@@ -0,0 +1,67 @@
+package de.dma.sayhello;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+
+public class StartActivity extends Activity implements TextToSpeech.OnInitListener {
+
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        tts = new TextToSpeech(this, (TextToSpeech.OnInitListener) this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonClick(View v) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        Button button = (Button) findViewById(R.id.button);
+
+        if (textView.getText() == getString(R.string.hello_world_english)) {
+            textView.setText(getString(R.string.hello_world_german));
+            button.setText(getString(R.string.button_german));
+        }
+        else {
+            textView.setText(getString(R.string.hello_world_english));
+            button.setText(getString(R.string.button_english));
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        tts.setLanguage(Locale.GERMAN);
+        tts.speak("Hallo Welt!", TextToSpeech.QUEUE_FLUSH, null, "Test");
+    }
+}
